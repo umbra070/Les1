5f1d94dcab77bcaa595d10f2bc2694830e1d668f
umbra070 <umbra070@yandex.ru>
2025-12-12 21:49:15 +0300

    Adding practical Variables and syntax

--- src/Main.java
+++ src/Main.java
@@ -23,10 +23,10 @@ void main() {
     var totalPersons = 0;
     IO.println("\n\rПеременные и синтаксис\n\r**************************************");
     IO.println(String.format("Ex1\n\rVariable dog = %f\n\rVariable cat = %f\n\rVariable paper = %d", dog, cat, paper));
-    IO.println(String.format("Ex2\n\rVariable dog + 4 = %f\n\rVariable cat + 4 = %f\n\rVariable paper + 4 = %d", dog+i, cat+i, paper+i));
-    IO.println(String.format("Ex3\n\rVariable dog - 3.5 = %f\n\rVariable cat - 1.6 = %f\n\rVariable paper - 7639 = %d", dog-3.5, cat-1.6, paper-7639));
+    IO.println(String.format("Ex2\n\rVariable dog + 4 = %f\n\rVariable cat + 4 = %f\n\rVariable paper + 4 = %d", dog = dog+i, cat = cat+i, paper = paper+i));
+    IO.println(String.format("Ex3\n\rVariable dog - 3.5 = %f\n\rVariable cat - 1.6 = %f\n\rVariable paper - 7639 = %d", dog-3.5, cat-1.6, paper = paper-7639));
     IO.println(String.format("Ext4\n\rOperation1: friend = %d\n\rOperation2: friend + 2 = %d\n\rOperation3: friend / 7 = %d",friend, friend=friend+2, friend/7));
-    IO.println(String.format("Ex5\n\rOperation1: frog = %f\n\rOperation2: frog * 10 = %f\n\rOperation3: frog / 3.5 = %f\n\rOperation4: frog + 4 = %f", frog, frog=frog*10, frog=frog/3.5, frog+4 ));
+    IO.println(String.format("Ex5\n\rOperation1: frog = %f\n\rOperation2: frog * 10 = %f\n\rOperation3: frog / 3.5 = %f\n\rOperation4: frog + 4 = %f", frog, frog=frog*10, frog=frog/3.5, frog = frog+4 ));
     IO.println(String.format("Ex6\n\rBoxer1 mass = %f\n\rBoxer2 mass = %f\r\nTotal mass of boxers = %f\n\rMass difference of boxers = %f\n\rEx7\n\rRemainder after division = %f",firstBoxerMass, secondBoxerMass, firstBoxerMass+secondBoxerMass, secondBoxerMass-firstBoxerMass, secondBoxerMass%firstBoxerMass));
     IO.println(String.format("Ex8.1\n\rВсего работников в компании %d\n\rEx8.2\n\rЕсли в компании работает %d человек, то всего %d часов работы может быть поделено между сотрудниками",totalPersons=totalHours/hourPerPerson, totalPersons=totalPersons+newPersons, totalHours/totalPersons));
     IO.println("**************************************");
